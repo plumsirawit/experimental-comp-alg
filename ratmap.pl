e0dc582584363cc3761c50fc99f45d7fc1500016
Sirawit <plurm2545@hotmail.com>
2023-11-20 01:09:39 +0100

    fix: introduce division

--- ratmap.pl
+++ ratmap.pl
@@ -12,13 +12,14 @@ eval(y, Out) :- Out = 1.
 eval(z, Out) :- Out = 0.
 eval(Term, Out) :- Term = T1 + T2, eval(T1, Out1), eval(T2, Out2), Out = Out1+Out2.
 eval(Term, Out) :- Term = T1 * T2, eval(T1, Out1), eval(T2, Out2), Out = Out1*Out2.
-eval(Term, Out) :- Term = T1 / T2, eval(T1, Out1), eval(T2, Out2), Out = Out1/Out2.
+eval(Term, Out) :- dif(T1, T2), Term = T1 / T2, eval(T1, Out1), eval(T2, Out2), Out = Out1/Out2.
+eval(Term, Out) :- Term = T / T, Out = 1.
 
 /*
   simplify(Term, Out): simplify numerical Term to a single numeric value.
 */
 
-simplify(Num, Out) :- number(Num), Out = Num.
+simplify(Atom, Out) :- atomic(Atom), Out = Atom.
 simplify(0+D, E) :- simplify(D, E).
 simplify(D+0, E) :- simplify(D, E).
 simplify(1*D, E) :- simplify(D, E).
@@ -30,13 +31,14 @@ simplify(0/_D, E) :- E = 0.
 simplify(D+F, E) :- D \= 0, F \= 0, simplify(D, E1), simplify(F, E2), ((D = E1, F = E2, E = E1 + E2); (dif(D, E1); dif(F, E2)), simplify(E1+E2, E)).
 simplify(D*F, E) :- D \= 0, F \= 0, D \= 1, F \= 1, simplify(D, E1), simplify(F, E2), ((D = E1, F = E2, E = E1 * E2); (dif(D, E1); dif(F, E2)), simplify(E1*E2, E)).
 simplify(D-F, E) :- simplify(D, E1), simplify(F, E2), ((D = E1, F = E2, E = E1 - E2); (dif(D, E1); dif(F, E2)), simplify(E1-E2, E)).
+simplify(D/F, E) :- D \= 0, F \= 0, simplify(D, E1), simplify(F, E2), ((D = E1, F = E2, E = D/F); (dif(D, E1); dif(F, E2)), simplify(E1/E2, E)).
 simplify(-D, E) :- simplify(D, E1), E = -E1.
 
 /*
   eval_zero(Term): check whether Term evaluates to zero or not.
 */
 
-eval_zero(Term) :- eval(Term, Out), simplify(Out, Out2), 0 is Out2.
+eval_zero(Term) :- simplify(Term, Out), eval(Out, Out2), 0 is Out2.
 
 /*
   needle_haystack(N, H): check whether an atom N exists in H. (https://stackoverflow.com/a/57602983/8757529)
@@ -48,14 +50,6 @@ needle_haystack(N,H) :- H=..[_|As],member(A,As),needle_haystack(N,A).
   eval_proj(TermX, TermY, TermZ, OutX, OutY, OutZ): evaluate [TermX; TermY; TermZ] (projective coordinates) into [OutX; OutY; OutZ].
 */
 
-eval_proj(TermX, TermY, TermZ, OutX, OutY, OutZ) :- 
-  number(TermX),
-  number(TermY),
-  number(TermZ),
-  OutX is TermX,
-  OutY is TermY,
-  OutZ is TermZ.
-
 eval_proj(TermX, TermY, TermZ, OutX, OutY, OutZ) :-
   TermX = Lambda * SubtermX,
   TermY = Lambda * SubtermY,
@@ -66,10 +60,20 @@ eval_proj(TermX, TermY, TermZ, OutX, OutY, OutZ) :-
   eval(TermX, InterX),
   eval(TermY, InterY),
   eval(TermZ, InterZ),
-  simplify(InterX, Inter2X),
-  simplify(InterY, Inter2Y),
-  simplify(InterZ, Inter2Z),
-  eval_proj(Inter2X, Inter2Y, Inter2Z, OutX, OutY, OutZ).
+  simplify(InterX, RetX),
+  simplify(InterY, RetY),
+  simplify(InterZ, RetZ),
+  (
+    (needle_haystack(oo, RetX), eval_proj(x*TermX, x*TermY, x*TermZ, OutX, OutY, OutZ));
+    (needle_haystack(oo, RetY), eval_proj(y*TermX, y*TermY, y*TermZ, OutX, OutY, OutZ));
+    (needle_haystack(oo, RetZ), eval_proj(z*TermX, z*TermY, z*TermZ, OutX, OutY, OutZ));
+    (RetX = 0, RetY = 0, RetZ = 0, (
+      eval_proj(TermX/x, TermY/x, TermZ/x, OutX, OutY, OutZ);
+      eval_proj(TermX/y, TermY/y, TermZ/y, OutX, OutY, OutZ);
+      eval_proj(TermX/z, TermY/z, TermZ/z, OutX, OutY, OutZ)
+    ));
+    (number(RetX), number(RetY), number(RetZ), OutX is RetX, OutY is RetY, OutZ is RetZ)
+  ).
 
 
 /*ratmap(TermX, TermY, TermZ) :-*/
